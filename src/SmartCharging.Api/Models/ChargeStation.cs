873ecbb8ecdcaefeb1206badd659ee209bc91ba7
Andres Rondon <an.rondon@gmail.com>
2023-06-29 11:29:08 -0400

    adding base models

---
+++ src/SmartCharging.Api/Models/ChargeStation.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace SmartCharging.Api.Models
+{
+    public class ChargeStation
+    {
+        [JsonProperty(PropertyName = "id")]
+        public required string Id { get; set; }
+
+        public required string Name { get; set; }
+
+        [MaxLength(5)]
+        public ICollection<Connector> Connectors { get; set; } = new List<Connector>();
+    }
+}
---
+++ src/SmartCharging.Api/Models/Connector.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace SmartCharging.Api.Models
+{
+    public class Connector
+    {
+        [JsonProperty(PropertyName = "id"), Range(1, 5)]
+        public int Id { get; set; }
+        
+        [Range(1, int.MaxValue, ErrorMessage = "Value for {0} must be minimun {1}")]
+        public int MaxCurrentInAmps { get; set; }
+    }
+}
